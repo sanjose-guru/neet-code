f82e775a81110c5a9fb8101aa0acd9c042232a9a
sanjose-guru <sanjose-guru@users.noreply.github.com>
2023-07-13 08:29:40 -0700

    added comments and fixed intend

--- group_anagrams/solution.py
+++ group_anagrams/solution.py
@@ -4,22 +4,24 @@
 import collections
 
 class Solution:
-  def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
-    res = collections.defaultdict(list) # mapping charCount to list of anagrams
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        res = collections.defaultdict(list) # mapping charCount to list of anagrams
 
-    for s in strs:
-      count = [0] * 26 # a...z
-      print(f"count: {count}")
+        for s in strs:
+            count = [0] * 26 # a...z. This list will be used a key in res dict
 
-      for c in s:
-        count[ord(c) - ord('a')] += 1
-      print(f"count: {count}")
+            # the char count list will be the key in res dict, 
+            # All strings with same char count will grouped in res
+            for c in s:
+                count[ord(c) - ord('a')] += 1
 
-      res[tuple(count)].append(s)
+            # All strings with same char count will grouped under same key
+            res[tuple(count)].append(s)
 
-    print(f"res: {res}")
-    return res.values()
+        return list(res.values())
 
 sol = Solution()
 print(f"sol: {sol.groupAnagrams(['eat','tea','tan','ate','nat','bat'])}")
+print(f"sol: {sol.groupAnagrams([''])}")
+print(f"sol: {sol.groupAnagrams(['a'])}")
 
